072ab03196118beb12c8643b41c5ec1f9d1ff533
Brahma <abhireddy.sre@gmail.com>
2022-06-13 06:00:05 -0700

    commiting files

---
+++ config/config.py
@@ -0,0 +1,7 @@
+topic_name = 'ECOM_OSCO_OSS_ORDER_EVENT_TOPIC_DEV1'
+bootstrap_servers = ['172.21.224.139:9092,172.21.224.140:9092,172.21.224.141:9092']
+elastic_index_name = 'test_kafka_index'
+elastic_hosts = ['http://elastic:Elastic_1234@localhost:9200/']
+kafka_consumer_offset = 'latest'
+kafka_consumer_group_id = 'default-group'
+kafka_message_decode_mode = 'utf-8'
